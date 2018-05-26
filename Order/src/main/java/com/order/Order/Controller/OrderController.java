98fcddfd1f27364266ebd5248abab6d3e1b89aad
Achint Mistry <achint@Achints-MacBook-Pro.local>
2018-05-26 16:31:14 +0530

    EMail Integrated

--- Order/src/main/java/com/order/Order/Controller/OrderController.java
+++ Order/src/main/java/com/order/Order/Controller/OrderController.java
@@ -2,11 +2,13 @@ package com.order.Order.Controller;
 
 import com.google.common.collect.Lists;
 import com.order.Order.DTO.OrderDto;
+import com.order.Order.MailSender;
 import com.order.Order.Model.OrderModel;
 import com.order.Order.Service.OrderInterface;
 import it.ozimov.springboot.mail.model.Email;
 import it.ozimov.springboot.mail.model.defaultimpl.DefaultEmail;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.persistence.criteria.Order;
+import java.time.LocalDate;
 import java.util.Collection;
 import java.util.List;
 
@@ -24,16 +27,21 @@ public class OrderController {
     @Autowired
     private OrderInterface orderInterface;
 
+    @Autowired
+    @Qualifier("mailsend")
+    public MailSender mailSender;
+
     @RequestMapping("place-order")
-    public String placeOrder(@RequestParam String emailUser, String productId, String userId, String merchantId, double cost) throws AddressException {
-        orderInterface.addProductToCart(new OrderDto(productId, userId, merchantId,cost));
-//        final Email email = DefaultEmail.builder()
-//                .from(new InternetAddress("Achint"))
-//                .to((Collection<InternetAddress>) new InternetAddress(emailUser))
-//                .subject("Testing")
-//                .body("Succes")
-//                .encoding("UTF-8").build();
-        return "Email Sent";
+    public OrderDto placeOrder(@RequestParam String emailUser, String productUrl, String productId, String userId, String merchantId, double cost) throws AddressException {
+
+        OrderDto orderDto = new OrderDto(productUrl, productId, userId, merchantId, cost);
+        orderInterface.addProductToCart(orderDto);
+        String from = "ecommercenoidea@gmail.com";
+        String to = "achintachu@gmail.com";
+        String subject = "Order Placed";
+        String body = "Congrats Order Placed, thanks for shopping with us";
+        mailSender.sendMail(from, to, subject, body);
+        return orderDto;
     }
 
     @RequestMapping("order-history")
--- Order/src/main/java/com/order/Order/Model/OrderModel.java
+++ Order/src/main/java/com/order/Order/Model/OrderModel.java
@@ -4,6 +4,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.time.LocalDate;
+import java.util.Date;
 
 @Entity
 @Table(name="orders")
@@ -16,19 +18,39 @@ public class OrderModel {
     private String productId;
     private String merchantId;
     private String userId;
+    private LocalDate orderDate;
+    private String productUrl;
     private double totalCost;
 
-    public OrderModel(String productId, String userId, String merchantId, double totalCost) {
+    public OrderModel(String productUrl, String productId, String userId, String merchantId, double totalCost) {
+        this.orderDate = java.time.LocalDate.now();
         this.productId = productId;
         this.merchantId = merchantId;
         this.userId = userId;
         this.totalCost = totalCost;
+        this.productUrl = productUrl;
+        this.orderDate = LocalDate.now();
     }
 
     public OrderModel() {
 
     }
 
+    public LocalDate getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDate orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public String getProductUrl() {
+        return productUrl;
+    }
+
+    public void setProductUrl(String productUrl) {
+        this.productUrl = productUrl;
+    }
 
     public String getProductId() {
         return productId;
